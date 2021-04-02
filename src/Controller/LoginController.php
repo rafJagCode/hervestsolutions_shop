125449633c94cd63f968e99ace96a9c53970dab3
rafJagCode <rafal.jagielski.1993@tlen.pl>
2021-04-02 13:18:39 +0200

    Added login and registration

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,16 +1,17 @@
 <?php
 
 namespace App\Controller;
+
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class LoginController extends AbstractController
 {
-	private $client;
+    private $client;
 
     public function __construct(HttpClientInterface $client)
     {
@@ -22,20 +23,88 @@ class LoginController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('/pages/account-login.twig', [
-            'controller_name' => 'LoginController',
+        return $this->render("/pages/account-login.twig", [
+            "controller_name" => "LoginController",
         ]);
     }
 
     /**
      * @Route("/sign-in", name="sign-in")
      */
-	public function signIn(Request $request): Response
-	{
-		dump($request);
-		exit;
-		return $this->render('/pages/account-login.twig', [
-            'controller_name' => 'LoginController',
-        ]);
-	}
+    public function signIn(Request $request): Response
+    {
+        try {
+            $response = $this->client->request(
+                "POST",
+                $_ENV["API_URL"] . "login_check",
+                [
+                    "json" => $request->request->all(),
+                ]
+            );
+        } catch (Exception $exception) {
+            throw $exception;
+        }
+
+        $statusCode = $statusCode = $response->getStatusCode();
+        $message = $this->getStatusCodeMessage($statusCode);
+
+        if ($statusCode === 200) {
+            return $this->render("/pages/account-dashboard.twig", [
+                "controller_name" => "LoginController",
+            ]);
+        } else {
+            return $this->render("/pages/account-login.twig", [
+                "controller_name" => "LoginController",
+                "loginMessage" => $message,
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(Request $request)
+    {
+        try {
+            $response = $this->client->request(
+                "POST",
+                $_ENV["API_URL"] . "register",
+                [
+                    "json" => $request->request->all(),
+                ]
+            );
+        } catch (Exception $exception) {
+            throw $exception;
+        }
+
+        $statusCode = $statusCode = $response->getStatusCode();
+        $message = $this->getStatusCodeMessage($statusCode);
+
+        if ($statusCode === 200) {
+            return $this->render("/pages/account-dashboard.twig", [
+                "controller_name" => "LoginController",
+            ]);
+        } else {
+            return $this->render("/pages/account-login.twig", [
+                "controller_name" => "LoginController",
+                "registerMessage" => $message,
+            ]);
+        }
+    }
+
+    public function getStatusCodeMessage(int $statusCode)
+    {
+        if ($statusCode === 200) {
+            $message = "Logged successfully";
+        } elseif ($statusCode === 400) {
+            $message = "Account already used";
+        } elseif ($statusCode === 401) {
+            $message = "Provided credentials were invalid";
+        } elseif ($statusCode === 500) {
+            $message = "Internal server error";
+        } else {
+            $message = "Something went wrong";
+        }
+        return $message;
+    }
 }
