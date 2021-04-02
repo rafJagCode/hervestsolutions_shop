198f6ebbf5bb2078450c81d3cdc8a539406ca1ee
rafJagCode <rafal.jagielski.1993@tlen.pl>
2021-04-02 09:55:09 +0200

    Sign in changes

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,22 +1,41 @@
 <?php
 
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class LoginController extends AbstractController
 {
+	private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     /**
      * @Route("/account-login", name="account-login")
      */
     public function index(): Response
     {
-		$obj =['1' => 'test'];
-        dump($obj);
         return $this->render('/pages/account-login.twig', [
             'controller_name' => 'LoginController',
         ]);
     }
+
+    /**
+     * @Route("/sign-in", name="sign-in")
+     */
+	public function signIn(Request $request): Response
+	{
+		dump($request);
+		exit;
+		return $this->render('/pages/account-login.twig', [
+            'controller_name' => 'LoginController',
+        ]);
+	}
 }
