dddb6bb17734533c828c1f897f3a1004d8b2ebd0
rafJagCode <rafal.jagielski.1993@tlen.pl>
2021-04-06 09:57:17 +0200

    Integrated cart page with backend

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -5,16 +5,40 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Exception;
 
 class CartController extends AbstractController
 {
+    private $client;
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     /**
      * @Route("/cart", name="cart")
      */
     public function index(): Response
     {
-        return $this->render('pages/cart.twig', [
-            'controller_name' => 'CartController',
-        ]);
+        try {
+            $response = $this->client->request(
+                "POST",
+                $_ENV["API_URL"] . "cart",
+                [
+                    "json" => ["user" => 2],
+                ]
+            );
+        } catch (Exception $exception) {
+            throw $exception;
+        }
+
+        $statusCode = $response->getStatusCode();
+        if ($statusCode === 200) {
+            return $this->render("pages/cart.twig", [
+                "controller_name" => "CartController",
+                "products" => $response->toArray(),
+            ]);
+        }
     }
 }
