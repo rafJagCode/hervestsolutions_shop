db7ce3803291e91195013c59ef7244cda3a3cec4
rafJagCode <rafal.jagielski.1993@tlen.pl>
2021-04-06 13:59:41 +0200

    Changed shop display

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -5,16 +5,39 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Exception;
 
 class ShopController extends AbstractController
 {
+    private $client;
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     /**
      * @Route("/shop", name="shop")
      */
     public function index(): Response
     {
-        return $this->render('pages/shop-table.twig', [
-            'controller_name' => 'ShopController',
-        ]);
+        try {
+            $response = $this->client->request(
+                "POST",
+                $_ENV["API_URL"] . "getproducts"
+            );
+        } catch (Exception $exception) {
+            throw $exception;
+        }
+
+        $statusCode = $response->getStatusCode();
+        $products = $response->toArray();
+
+        if ($statusCode === 200) {
+            return $this->render("pages/shop-grid-4-columns-full.twig", [
+                "controller_name" => "ShopController",
+                "productsB" => $products,
+            ]);
+        }
     }
 }
