8b0db9d10cfabcaf4057ab966578084c602ae3c7
rafJagCode <rafal.jagielski.1993@tlen.pl>
2021-04-09 12:38:36 +0200

    Moved assets to public folder

--- assets/js/logout.js
+++
@@ -1,6 +0,0 @@
-const logout = () => {
-	let date = new Date().toGMTString();
-	let cookieName = 'X-AUTH-TOKEN';
-	let domain = 'localhost';
-	document.cookie = `${cookieName}= ;expires = ${date};domain=${domain};path=/`;
-}
