e3c7b4983250dea7cf9bfe38d0aca39a274faa99
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2021-04-20 12:42:41 +0200

    Removed old logout

--- public/assets/js/logout.js
+++
@@ -1,5 +0,0 @@
-const logout = () => {
-	let cookieName = 'X-AUTH-TOKEN';
-	let domain = 'redparts.test';
-	Cookies.set(cookieName, '', { expires: -1, path: '/', domain: domain, sameSite: 'lax'});
-}
