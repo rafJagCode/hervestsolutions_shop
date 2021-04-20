b45ff6e3ec769527ae7793eb03bd0549826eafa6
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2021-04-20 15:39:37 +0200

    Added customer search table

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -18,6 +18,22 @@ class CustomerController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/customer-search", name="customer-search")
+     */
+    public function searchCustomer(): Response
+    {
+		$user = (object)['firstName' => 'Jan', 'lastName' => 'Kowalski', 'address' => 'Miasto 04-322 ul. Wesoła 5', 'telephone' => '653 532 256', 'email' => 'jankow@test.com'];
+		$users = [];
+		for ($i=0; $i < 10; $i++) {
+			array_push($users, $user);
+		}
+        return $this->render('pages/customer-search.twig', [
+            'controller_name' => 'CustomerController',
+			'users' => $users,
+        ]);
+    }
+
 	/**
 	 * @Route("/generate-password", name="generate-password")
 	 */
