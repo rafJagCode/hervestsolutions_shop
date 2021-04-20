a45c2f9ab30d363619f401896ace6b0310f1b58f
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2021-04-20 15:40:21 +0200

    Changed display in admin dashboard to rendering components

--- public/assets/js/admin-controls.js
+++ public/assets/js/admin-controls.js
@@ -1,10 +1,28 @@
-const csvControls = $('.admin-section__csv-controls');
 
 const changeArrow = () =>{
 	const arrow = $('.admin-section__arrow');
 	arrow.toggleClass('fa-chevron-right fa-chevron-down')
 }
 const openCsvSection = ()=>{
+	const csvControls = $('.admin-section__csv-controls');
 	changeArrow();
 	csvControls.toggleClass('section--open');
+}
+const renderCustomerSearch = ()=>{
+	const accountBody = $('#account-body');
+	$.ajax({
+		url: '/customer-search',
+		success: function(data){
+			accountBody.html(data);
+		}
+	})
+}
+const renderCustomerCreator= ()=>{
+	const accountBody = $('#account-body');
+	$.ajax({
+		url: '/customer-creator',
+		success: function(data){
+			accountBody.html(data);
+		}
+	})
 }
