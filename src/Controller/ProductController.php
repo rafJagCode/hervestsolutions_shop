13caec5b618be0ecd00f65b2063b4d16665018c2
rafJagCode <rafal.jagielski.1993@tlen.pl>
2021-05-04 13:56:23 +0200

    Added searching by number

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,55 +8,66 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\ProductsCategoriesGetter;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Exception;
 
 class ProductController extends AbstractController
 {
-	private $client;
-	private $productsCategoriesGetter;
-	public function __construct(
-		HttpClientInterface $client,
-		ProductsCategoriesGetter $productsCategoriesGetter,
-	) {
-		$this->client = $client;
-		$this->productsCategoriesGetter = $productsCategoriesGetter;
-	}
+    private $client;
+    private $productsCategoriesGetter;
+    public function __construct(
+        HttpClientInterface $client,
+        ProductsCategoriesGetter $productsCategoriesGetter
+    ) {
+        $this->client = $client;
+        $this->productsCategoriesGetter = $productsCategoriesGetter;
+    }
 
-	/**
-	 * @Route("/product/{id}", name="product")
-	 */
-	public function index(
-		$id
-	): Response {
-		$newest = $this->productsCategoriesGetter->getNewest();
-		$response = $this->client->request(
-			"POST",
-			$_ENV["API_URL"] . "getproduct",
-			[
-				"json" => ["id" => $id],
-			]
-		);
+    /**
+     * @Route("/product/{id}", name="product")
+     */
+    public function index($id): Response
+    {
+        $newest = $this->productsCategoriesGetter->getNewest();
+        $response = $this->client->request(
+            "POST",
+            $_ENV["API_URL"] . "getproduct",
+            [
+                "json" => ["id" => $id],
+            ]
+        );
 
+        if ($response->getStatusCode() === 200) {
+            $product = $response->toArray()[0];
+            return $this->render("pages/product-full.twig", [
+                "controller_name" => "ProductController",
+                "product" => $product,
+                "selectedFourProducts" => $newest,
+            ]);
+        }
+    }
+    /**
+     * @Route("/search-in-products", name="search-in-products")
+     */
+    public function searchInProducts(Request $request)
+    {
+        $requestContent = json_decode($request->getContent(), true);
 
-		if ($response->getStatusCode() === 200) {
-			$product = $response->toArray()[0];
-			return $this->render("pages/product-full.twig", [
-				"controller_name" => "ProductController",
-				"product" => $product,
-				"selectedFourProducts" => $newest
-			]);
-		}
-	}
-	/**
-	 * @Route("/search-in-products/{input}", name="search-in-products")
-	 */
-	public function searchInProducts($input)
-	{
+        $response = $this->client->request(
+            "POST",
+            $_ENV["API_URL"] . "getproductbynumber",
+            [
+                "json" => ["number" => $requestContent["number"]],
+            ]
+        );
 
-		$products = $this->productsCategoriesGetter->getNewest();
-		$regex = "/$input/";
-		$filtered = array_filter($products, function($product) use($regex){
-			return preg_match($regex, $product["name"]);
-		});
-		return new JsonResponse($filtered);
-	}
+        if ($response->getStatusCode() === 200) {
+            try {
+                $products = $response->toArray();
+            } catch (Exception) {
+                $products = [];
+            }
+            return new JsonResponse($products);
+        }
+    }
 }
--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -9,23 +9,30 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class SearchController extends AbstractController
 {
-	private $http;
-	public function __construct(HttpClientInterface $http)
-	{
-		$this->http = $http;
-	}
+    private $http;
+    public function __construct(HttpClientInterface $http)
+    {
+        $this->http = $http;
+    }
     /**
      * @Route("/search-results/{input}", name="search-results")
      */
     public function searchProducts($input): Response
     {
-		$response = $this->http->request('GET', 'http://redparts.test/search-in-products/' . $input);
+        $response = $this->http->request(
+            "POST",
+            "http://redparts.test/search-in-products",
+            [
+                "json" => ["number" => $input],
+            ]
+        );
+        if ($response->getStatusCode() === 200) {
+            $products = $response->toArray();
 
-		if($response->getStatusCode()===200){
-			return $this->render('components/search-results.twig', [
-			'controller_name' => 'SearchController',
-			'searchResults' => $response->toArray(),
-			]);
-		}
+            return $this->render("components/search-results.twig", [
+                "controller_name" => "SearchController",
+                "searchResults" => $products,
+            ]);
+        }
     }
 }
