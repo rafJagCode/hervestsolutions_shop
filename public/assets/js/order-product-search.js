386d00e12771daf11bdc5f85f9804afc2e0fa689
rafJagCode <rafal.jagielski.1993@tlen.pl>
2021-05-04 16:02:02 +0200

    Working  on searching products for customers

--- public/assets/js/order-product-search.js
+++ public/assets/js/order-product-search.js
@@ -1,10 +1,11 @@
 $( document ).ready(function() {
 	const searchInput = $('.order-product-search__input');
 	const dropdown = $('.order-product-search__dropdown');
+	const dropdownArea = $('order-product-search__body');
 	searchInput.focus(function(){
 		dropdown.addClass('order-product-search__dropdown--open');
 	});
-	searchInput.blur(()=>{
+	dropdownArea.blur(()=>{
 		dropdown.removeClass('order-product-search__dropdown--open');
 	});
 	searchInput.on('input', ()=>{
