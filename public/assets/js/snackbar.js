b4bc0afa645638ad863e98c83ac6ba88b706e383
rafJagCode <rafal.jagielski.1993@tlen.pl>
2021-05-05 15:16:58 +0200

    Changes in snackbar

--- public/assets/js/snackbar.js
+++ public/assets/js/snackbar.js
@@ -1,20 +1,9 @@
 $(document).ready(function () {
   (function showSnackbar() {
-    const observer = new MutationObserver(function (mutations) {
-      const snackbar = $(".snackbar");
-      if (snackbar) {
-        snackbar.addClass("snackbar--show");
-        setTimeout(function () {
-          snackbar.removeClass("snackbar--show");
-        }, 5000);
-        observer.disconnect();
-      }
-    });
-    observer.observe(document, {
-      attributes: false,
-      childList: true,
-      characterData: false,
-      subtree: true,
-    });
+    const snackbar = $(".snackbar");
+    snackbar.addClass("snackbar--show");
+    setTimeout(function () {
+      snackbar.removeClass("snackbar--show");
+    }, 5000);
   })();
 });
