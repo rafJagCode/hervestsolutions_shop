ec2fe4e21d4bfbf33b7bdd143969cb5b04ce5b5e
rafJagCode <rafal.jagielski.1993@tlen.pl>
2021-05-05 10:19:47 +0200

    Finished admin customer product search

--- public/assets/js/order-product-search.js
+++ public/assets/js/order-product-search.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  const searchIcon = $(".order-product-search__search-icon");
   const dropdownArea = $(".order-product-search__body");
   const searchInput = $(".order-product-search__input");
   const dropdown = $(".order-product-search__dropdown");
@@ -6,10 +7,15 @@ $(document).ready(function () {
   searchInput.on("input focus", () => {
     const input = searchInput.val();
     if (!input.length) return;
+    console.log(searchIcon);
+    searchIcon.removeClass("fa-search");
+    searchIcon.addClass("fa-spinner fa-pulse");
     $.ajax({
       url: `/search-results/${input}`,
       success: function (data) {
         dropdown.html(data);
+        searchIcon.removeClass("fa-spinner fa-pulse");
+        searchIcon.addClass("fa-search");
       },
     });
   });
