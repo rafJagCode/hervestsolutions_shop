f5e50fb1da8116c310eeda89c4e58a84619b6915
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-09-30 22:44:53 +0200

    Cleaned ProducersController

--- src/Controller/ProducersController.php
+++ src/Controller/ProducersController.php
@@ -5,19 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
-use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\Producer;
 
 class ProducersController extends AbstractController
 {
-	private $client;
-	private $em;
 
-	public function __construct(HttpClientInterface $client, EntityManagerInterface $entityManager)
+	public function __construct()
 	{
-		$this->client = $client;
-		$this->em = $entityManager;
 	}
 
 	/**
@@ -25,10 +18,8 @@ class ProducersController extends AbstractController
 	 */
 	public function index(): Response
 	{
-		$producers = $this->em->getRepository(Producer::class)->findAll();
 		return $this->render("pages/producers.twig", [
 			"controller_name" => "ProducersController",
-			"producers" => $producers,
 		]);
 	}
 }
