11d26d705208898c98ed22d761384824e3691bf9
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-09-30 22:43:24 +0200

    Cleaned IndexController

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,29 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\ProductGetter;
 
 class IndexController extends AbstractController
 {
-    private $productGetter;
-    public function __construct(ProductGetter $productGetter)
+    public function __construct()
     {
-        $this->productGetter = $productGetter;
+
     }
     /**
      * @Route("/", name="index")
      */
     public function index(): Response
     {
-        $newest = $this->productGetter->getNewest();
-        $popular = $this->productGetter->getPopular();
-        $bestSellers = $this->productGetter->getBestSellers();
-
         return $this->render("pages/index.twig", [
             "controller_name" => "IndexController",
-            "newest" => $newest,
-            "popular" => $popular,
-            "bestSellers" => $bestSellers
         ]);
     }
 }
