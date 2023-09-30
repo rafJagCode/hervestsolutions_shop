f3e6e2a53dd4b8dffd6b26b9a4b94cfc93439695
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-09-30 22:45:36 +0200

    Cleaned QuickviewController

--- src/Controller/QuickviewController.php
+++ src/Controller/QuickviewController.php
@@ -5,17 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 use App\Service\ProductGetter;
 
 class QuickviewController extends AbstractController
 {
-	private $client;
 	private $productGetter;
 
-	public function __construct(HttpClientInterface $client, ProductGetter $productGetter)
+	public function __construct(ProductGetter $productGetter)
 	{
-		$this->client = $client;
 		$this->productGetter = $productGetter;
 	}
 
