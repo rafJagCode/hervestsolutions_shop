c23bed683ec9e7111efcb3c036a4d61ca765e353
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-09-30 23:03:09 +0200

    Added query builders for searching in products

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -19,32 +19,108 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
-    // /**
-    //  * @return Product[] Returns an array of Product objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @return Product[] Returns an array of Product objects
+     */
+    public function findNewest()
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
+            ->orderBy('p.addedDate', 'DESC')
             ->setMaxResults(10)
             ->getQuery()
             ->getResult()
         ;
     }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?Product
+    /**
+     * @return Product[] Returns an array of Product objects
+     */
+    public function findBestSellers()
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
+            ->orderBy('p.sold', 'DESC')
+            ->setMaxResults(10)
             ->getQuery()
-            ->getOneOrNullResult()
+            ->getResult()
         ;
     }
-    */
+
+    /**
+     * @return Product[] Returns an array of Product objects
+     */
+    public function findPopular()
+    {
+        return $this->createQueryBuilder('p')
+            ->orderBy('p.visited', 'DESC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * @return Product[] Returns an array of Product objects
+     */
+    public function findByOptions($options, $returnQuery=false)
+    {
+		$default = ['categoryId' => null, 'producerIds' => null, 'excludedId' => null, 'promotion' => null, 'phraze' => null, 'priceFrom' => null, 'priceTo' => null];
+  		$options = (object) array_merge($default, $options);
+
+        $qb = $this->createQueryBuilder('p');
+
+		if(!is_null($options->categoryId)){
+			$qb->andWhere('p.category = :categoryId')
+            ->setParameter('categoryId', $options->categoryId);
+		};
+
+		if(!is_null($options->producerIds)){
+			$qb->andWhere('p.producer IN (:producerIds)')
+			->setParameter('producerIds', $options->producerIds);
+		};
+
+		if(!is_null($options->excludedId)){
+			$qb->andWhere('p.id != :excludedId')
+            ->setParameter('excludedId', $options->excludedId);
+		}
+
+		if(!is_null($options->promotion)){
+			$qb->andWhere('p.promotion = :promotion')
+            ->setParameter('promotion', $options->promotion);	
+		}
+
+		if(!is_null($options->phraze)){
+			$qb->andWhere('p.name LIKE :phraze')
+			->setParameter('phraze', '%'.$options->phraze.'%');
+		}
+
+		if(!is_null($options->priceFrom)){
+			$qb->andWhere('p.price >= :priceFrom')
+			->setParameter('priceFrom', $options->priceFrom);
+		}
+
+		if(!is_null($options->priceTo)){
+			$qb->andWhere('p.price <= :priceTo')
+			->setParameter('priceTo', $options->priceTo);
+		}
+
+		if($returnQuery) return $qb;
+
+		return $qb
+		->getQuery()
+		->getResult();
+    }
+	   
+    /**
+     * @return Product[] Returns an array of Product objects
+     */
+    public function findByPhraze($phraze)
+    {
+        return $this->createQueryBuilder('p')
+			->andWhere('p.name LIKE :phraze')
+			->setParameter('phraze', '%'.$phraze.'%')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
 }
