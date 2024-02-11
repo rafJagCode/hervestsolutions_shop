8259b065cbab74899a4c8e8c501079f103d83ac1
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-02-11 02:52:20 +0100

    Changed snackbar handling method

--- public/assets/js/snackbar.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  (function showSnackbar() {
-    const snackbar = $(".snackbar");
-    snackbar.addClass("snackbar--show");
-    setTimeout(function () {
-      snackbar.removeClass("snackbar--show");
-    }, 5000);
-  })();
-});
